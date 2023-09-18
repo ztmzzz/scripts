ca04d4e1ae404d71200467563225e8588778dfcf
ztmzzz <41851415+ztmzzz@users.noreply.github.com>
2023-09-18 13:49:56 +0000

    Update sfacg.js

--- shadowrocket/sfacg/sfacg.js
+++ shadowrocket/sfacg/sfacg.js
@@ -2,30 +2,21 @@ let VAL_signcookie=$persistentStore.read("cookie_sfacg");
 let VAL_SFSecurity=$persistentStore.read("SFSecurity_sfacg");
 let VAL_Authorization=$persistentStore.read("Authorization_sfacg");
 let VAL_UA=$persistentStore.read("UA_sfacg");
-console.log(VAL_signcookie);
 const url = "https://api.sfacg.com/user/signInfo/";
-const method = "PUT";
 const headers = {"Host": "api.sfacg.com","Content-Length": 0,"User-Agent": VAL_UA,"Cookie": VAL_signcookie,"SFSecurity": VAL_SFSecurity,"Authorization": VAL_Authorization};
 
 const myRequest = {
     url: url,
-    headers: headers, // Optional.
+    headers: headers, 
     body: ""
 };
 
-$httpClient.put(myRequest).then(response => {
-    // response.statusCode, response.headers, response.body
-    // console.log(response.body);
-    // $notify("Title", "Subtitle", response.body); // Success!
-    let res = JSON.parse(response.body);
-    if(res['data']===null){
-        console.log(res['status']['msg']);
+$httpClient.put(myRequest,(err,response,data) =>{
+    if(data===null){
+        console.log(response['status']['msg']);
     }else {
-        console.log('获取代券数:'+res['data']['coupons']);
+        console.log('获取代券数:'+data['coupons']);
     }
-    $done();
-}, reason => {
-    // reason.error
-    console.log("SFACG"+ "签到失败"+ reason.error); // Error!
-    $done();
-});
+    $done();})
+    
+
