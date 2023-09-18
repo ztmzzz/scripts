ae3ddd04316808aa83942c3be45c28e4a0e2a8e0
ztmzzz <41851415+ztmzzz@users.noreply.github.com>
2023-09-18 13:19:40 +0000

    Update sfacg.js

--- shadowrocket/sfacg/sfacg.js
+++ shadowrocket/sfacg/sfacg.js
@@ -2,7 +2,7 @@ let VAL_signcookie=$persistentStore.read("cookie_sfacg");
 let VAL_SFSecurity=$persistentStore.read("SFSecurity_sfacg");
 let VAL_Authorization=$persistentStore.read("Authorization_sfacg");
 let VAL_UA=$persistentStore.read("UA_sfacg");
-
+console.log(VAL_signcookie);
 const url = "https://api.sfacg.com/user/signInfo/";
 const method = "PUT";
 const headers = {"Host": "api.sfacg.com","Content-Length": 0,"User-Agent": VAL_UA,"Cookie": VAL_signcookie,"SFSecurity": VAL_SFSecurity,"Authorization": VAL_Authorization};
